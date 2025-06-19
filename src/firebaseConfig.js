323d5abddf138eac2f30b38b3cac459487568c1c
Anish <anish22075@iiitd.ac.in>
2025-06-19 09:28:02 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBs0QNsJu_dGMY-2K3YoYBrCdVytgYY68o",
-  authDomain: "research-to-commercialization.firebaseapp.com",
-  projectId: "research-to-commercialization",
-  storageBucket: "research-to-commercialization.firebasestorage.app",
-  messagingSenderId: "732981995220",
-  appId: "1:732981995220:web:fb17ce2f8da15f91343d41",
-  measurementId: "G-9SDGNPW8RC"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
