a0b85c403362fce18c2b45b01fd8ee795e46eb41
Anish <anish22075@iiitd.ac.in>
2025-07-29 18:38:40 +0530

    Update StudyDetails.js

--- src/pages/UserStudyDetail/StudyDetails.js
+++ src/pages/UserStudyDetail/StudyDetails.js
@@ -153,7 +153,7 @@ const StudyDetails = ({ study }) => {
                     {study.documents.map((doc, index) => (
                       <ListItem key={index} disablePadding sx={{mb: 1}}>
                           <MuiLink 
-                            href={`http://r2c.iiitd.edu.in/${doc.file_location}`} 
+                            href={`/api/${doc.file_location}`} 
                             target="_blank" 
                             rel="noopener noreferrer" 
                             sx={{ textDecoration: 'none', color: 'inherit', width: '100%' }}
