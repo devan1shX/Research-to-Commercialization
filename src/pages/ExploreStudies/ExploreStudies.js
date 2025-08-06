bbc26425acd13953c2745d936b81260d9fbadf01
Anish <anish22075@iiitd.ac.in>
2025-08-06 08:00:18 +0530

    Update ExploreStudies.js

--- src/pages/ExploreStudies/ExploreStudies.js
+++ src/pages/ExploreStudies/ExploreStudies.js
@@ -527,16 +527,16 @@ const ExploreStudies = () => {
           </Box>
         </Box>
 
-        // <ActiveFilters
-        //   selectedFields={selectedGenres}
-        //   searchQuery={searchQuery}
-        //   selectedDateRange={selectedDateRange}
-        //   onClearField={handleRemoveSelectedGenre}
-        //   onClearSearch={handleClearSearch}
-        //   onClearDateRange={handleClearDateRange}
-        //   fields={allAvailableGenresForSelector}
-        //   dateRangeOptions={dateRangeOptions}
-        // />
+        {/* <ActiveFilters
+          selectedFields={selectedGenres}
+          searchQuery={searchQuery}
+          selectedDateRange={selectedDateRange}
+          onClearField={handleRemoveSelectedGenre}
+          onClearSearch={handleClearSearch}
+          onClearDateRange={handleClearDateRange}
+          fields={allAvailableGenresForSelector}
+          dateRangeOptions={dateRangeOptions}
+        /> */}
 
         <Box
           sx={{
@@ -556,4 +556,5 @@ const ExploreStudies = () => {
 };
 
 export default ExploreStudies;
+
 
